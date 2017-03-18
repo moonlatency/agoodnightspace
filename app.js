80f51d12edd61a268e86d52d20150ca1f77b8ca0
Brook Thomas <brookthomas@gmail.com>
2017-03-18 15:40:13 -0700

    removing handlebars for now

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var handlebars = require('handlebars');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,7 +13,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
