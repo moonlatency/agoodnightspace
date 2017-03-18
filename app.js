92aa81d0e35df1bd2abd455ad022334ae8318c9e
Brook Thomas <brookthomas@gmail.com>
2017-03-18 15:38:43 -0700

    fixed typo

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var handlebars = require('handlebars');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var home = erquire('./routes/home');
+var home = require('./routes/home');
 
 var app = express();
 
