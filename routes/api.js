f6eac2e616d221a52c854cc6dc32e46219d5d70e
lindsey anne <lindsey.hogg@grandrounds.com>
2017-03-19 05:00:07 -0700

    WIP: new api things

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,27 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/increment-bed-available', function(req, res, next) {
-    res.send('??');
+var express    = require('express');
+var router     = express.Router();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'ec2-user',
+  password : 'sfhacks',
+  database : 'ags_prod'
 });
+
+connection.connect(function(err){});
+
+router.get('/increment-beds-occupied', function (req, res, next) {
+  connection.query("update gs_beds_available set current_capacity = current_capacity + 1 where shelter_id = 1", function(err, results){
+    connection.end();
+  });
+  res.send('Incremented occupied beds')
+})
+
+router.get('/decrement-beds-occupied', function (req, res, next) {
+  connection.query("update gs_beds_available set current_capacity = current_capacity - 1 where shelter_id = 1", function(err, results){
+    connection.end();
+  });
+  res.send('Decremented occupied beds')
+})
 
 module.exports = router;
