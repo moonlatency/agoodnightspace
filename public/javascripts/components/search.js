e87edf7d1a1dfeaad157747373e5f436d233dfce
Brook Thomas <brookthomas@gmail.com>
2017-03-19 06:20:23 -0700

    with dummy data

--- public/javascripts/components/search.js
+++ public/javascripts/components/search.js
@@ -2,15 +2,31 @@ let search = new Vue({
     el: '#search',
     data: {
         results: [
-            { shelter_id: '1', name: 'shelter1', address: '2342 Foggy Bottom Way', beds_available: 32, filters: {women: true} }, 
-            { shelter_id: '2', name: 'shelter2', address: '2342 Foggy Bottom Way', beds_available: 0, filters: { women: true} },
-            { shelter_id: '3', name: 'mens_shelter', address: '2342 Foggy Bottom Way', beds_available: 5, filters: {women: false} },
-        ],
+{ shelter_id: '1', name: 'San Francisco Homeless Outreach', address: '283 Steiner St.', beds_available: 0, filters: {women: false, open: false} }, 
+{ shelter_id: '2', name: 'Better Choices', address: '183 Mission St.', beds_available: 3, filters: {women: false, open: false} },
+{ shelter_id: '3', name: 'Catholic Charities of San Francisco', address: '17 Capp St.', beds_available: 39, filters: {women: false, open: false, showers: true} },
+{ shelter_id: '4', name: 'Care Outreach Services', address: '1774 Hayes St.', beds_available: 0, filters: {women: false, open: false} },
+{ shelter_id: '5', name: 'Helping Hands Shelter', address: '932 Martin Luther King Blvd.', beds_available: 77, filters: {women: false, open: true} },
+{ shelter_id: '6', name: 'San Francisco Women\'s Shelter', address: '238 Argent Ct.', beds_available: 8, filters: {women: true, open: true} },
+{ shelter_id: '7', name: 'San Francisco Men\'s Shelter', address: '241 Argent Ct.', beds_available: 3, filters: {men: true, women: false, open: true} },
+{ shelter_id: '8', name: 'Sunset Baptist Church', address: '1438 Ulloa St.', beds_available: 0, filters: {women: true, open: true, showers: true} },
+{ shelter_id: '9', name: 'Jazzy\'s Place', address: '238 Mission St.', beds_available: 11, filters: {women: false, open: true, showers: true, lgbtq: true} },
+{ shelter_id: '10', name: 'Salvation Army', address: '1749 Van Ness St.', beds_available: 183, filters: {women: false, open: true, showers: true, lgbtq: false} }
+],
         filteredResults: [
-            { shelter_id: '1', name: 'shelter1', address: '2342 Foggy Bottom Way', beds_available: 32, filters: {women: true} }, 
-            { shelter_id: '2', name: 'shelter2', address: '2342 Foggy Bottom Way', beds_available: 0, filters: { women: true} },
-            { shelter_id: '3', name: 'mens_shelter', address: '2342 Foggy Bottom Way', beds_available: 5, filters: {women: false} },
-        ],
+{ shelter_id: '1', name: 'San Francisco Homeless Outreach', address: '283 Steiner St.', beds_available: 0, filters: {women: false, open: false} }, 
+{ shelter_id: '2', name: 'Better Choices', address: '183 Mission St.', beds_available: 3, filters: {women: false, open: false} },
+{ shelter_id: '3', name: 'Catholic Charities of San Francisco', address: '17 Capp St.', beds_available: 39, filters: {women: false, open: false, showers: true} },
+{ shelter_id: '4', name: 'Care Outreach Services', address: '1774 Hayes St.', beds_available: 0, filters: {women: false, open: false} },
+{ shelter_id: '5', name: 'Helping Hands Shelter', address: '932 Martin Luther King Blvd.', beds_available: 77, filters: {women: false, open: true} },
+{ shelter_id: '6', name: 'San Francisco Women\'s Shelter', address: '238 Argent Ct.', beds_available: 8, filters: {women: true, open: true} },
+{ shelter_id: '7', name: 'San Francisco Men\'s Shelter', address: '241 Argent Ct.', beds_available: 3, filters: {men: true, women: false, open: true} },
+{ shelter_id: '8', name: 'Sunset Baptist Church', address: '1438 Ulloa St.', beds_available: 0, filters: {women: true, open: true, showers: true} },
+{ shelter_id: '9', name: 'Jazzy\'s Place', address: '238 Mission St.', beds_available: 11, filters: {women: false, open: true, showers: true, lgbtq: true} },
+{ shelter_id: '10', name: 'Salvation Army', address: '1749 Van Ness St.', beds_available: 183, filters: {women: false, open: true, showers: true, lgbtq: false} }
+
+
+],
         checkedFilters: []
     },
     methods: {
@@ -21,7 +37,7 @@ let search = new Vue({
                 debugger;
                 switch ( f ) {
                     case 'open':
-                        filtered = filtered.filter( (i) => true );
+                        filtered = filtered.filter( (i) => i.filters.open );
                         break;
 
                     case 'beds':
@@ -30,6 +46,18 @@ let search = new Vue({
 
                     case 'women':
                         filtered = filtered.filter( ( i ) => i.filters.women );
+                        break;
+
+                    case 'showers':
+                        filtered = filtered.filter( ( i ) => i.filters.showers );
+                        break;
+
+                    case 'men':
+                        filtered = filtered.filter( ( i ) => i.filters.men );
+                        break;
+
+                    case 'lgbtq':
+                        filtered = filtered.filter( ( i ) => i.filters.lgbtq );
                         break;
                 }
             });
