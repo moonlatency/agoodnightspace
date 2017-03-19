aecd806474887b701fa5af9d34e3b945cc417b88
Brook Thomas <brookthomas@gmail.com>
2017-03-18 17:27:29 -0700

    endpoints set

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('wtf');
+  //res.render('wtf');
+  res.send('landing/login');
 });
 
+router.get('/register', function(req, res, next) {
+  //res.render('wtf');
+  res.send('register');
+});
+
+router.get('/home', function(req, res, next) {
+  //res.render('wtf');
+  res.send('home');
+});
+
+router.get('/search', function(req, res, next) {
+  //res.render('wtf');
+  res.send('search');
+});
 module.exports = router;
