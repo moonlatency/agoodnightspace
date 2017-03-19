302c614a022c1a8f7f02ee901aaad0bbaec94861
andrewlesondak <arlesondak@gmail.com>
2017-03-19 03:20:09 -0700

    Adding shelter pages.

---
+++ public/javascripts/components/myShelterInfo.js
@@ -0,0 +1,14 @@
+let myShelterInfo = new Vue({
+    el: '#my-shelter-info',
+    data: {
+        name: "SF Shelter",
+        address: "Folsom Street",
+        city: "San Francisco",
+        state: "California",
+        zip: "94103",
+        phone: "415-555-5555",
+        description: "Give me your tired, your poor..",
+        hours: "9pm - 7am",
+        max_capacity: "50 Beds"
+    },
+});
