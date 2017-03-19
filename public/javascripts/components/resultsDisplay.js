4ed933749118f9ac081fe290cb195b1aa0b0842d
Brook Thomas <brookthomas@gmail.com>
2017-03-19 00:25:46 -0700

    resultsdisplay styling

--- public/javascripts/components/resultsDisplay.js
+++ public/javascripts/components/resultsDisplay.js
@@ -2,22 +2,29 @@ let resultsDisplay = new Vue({
     el: '#results-display',
     data: {
         results: [
-            { shelter_id: '1', name: 'shelter1', address: 'hoodville' }, 
-            { shelter_id: '2', name: 'shelter2', address: 'savagetown' }
+            { shelter_id: '1', name: 'shelter1', address: '2342 Foggy Bottom Way', beds_available: 32, filters: [] }, 
+            { shelter_id: '2', name: 'shelter1', address: '2342 Foggy Bottom Way', beds_available: 32, filters: [] },
+            { shelter_id: '3', name: 'shelter1', address: '2342 Foggy Bottom Way', beds_available: 32, filters: [] },
         ]
     },
     methods: {
-    
+     
     },
     components: {
         'shelter-result': {
             template: `
                 <div class='shelter-result'>
-                    <span>{{ name }}</span>
-                    <span>{{ address }}</span>
+                    <div class='left'>
+                        <div class='name'>\{{ name }}</div>
+                        <div class='address'>\{{ address }}</div>
+                    </div>
+                    <div class='right'>
+                        <div class='beds-available'>\{{ beds_available }}</div>
+                        <span>beds available</span>
+                    </div>
                 </div>
             `,
-            props: ['shelter_id', 'name', 'address']
+            props: ['shelter_id', 'name', 'address', 'beds_available']
         } 
     },
 });
