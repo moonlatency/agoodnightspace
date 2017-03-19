4888c63432b3c3c1a2e2788b8b298c534042c814
Brook Thomas <brookthomas@gmail.com>
2017-03-19 05:02:01 -0700

    ux

--- public/javascripts/components/bedsController.js
+++ public/javascripts/components/bedsController.js
@@ -6,9 +6,13 @@ let bedsController = new Vue({
     methods: {
         increment: function () {
             this.beds_available++;
+            $('.fa-bed').css('color', '#4682b4');
         },
         decrement: function () {
             this.beds_available = (--this.beds_available < 0) ? 0 : this.beds_available;
+            if (  this.beds_available == 0 ) {
+                $('.fa-bed').css('color', 'red');
+            }
         }
     }
 });
