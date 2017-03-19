51191a989e6626793520f5d664d2b32366f6bcaa
Brook Thomas <brookthomas@gmail.com>
2017-03-19 03:51:11 -0700

    adding api route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var exphbs  = require('express-handlebars');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/increment-bed-available', function(req, res, next) {
+    res.send('??');
+});
+
+module.exports = router;
